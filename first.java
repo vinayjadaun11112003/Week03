344ec6249e1df19b11da32232589d995a7f12b8e
vinayjadaun11112003 <vinayjadaun11112003@gmail.com>
2025-01-27 10:23:49 +0530

    initial commit in the main branch containing first java file which has some info

---
+++ first.java
@@ -0,0 +1,6 @@
+public class first {
+    public static void main(String[] args){
+        System.out.println("This is the Week 03 repository for Capgemini training containing all files and projects according to the Day they were Assigned");
+        System.out.println("Happy Coding : ) ");
+    }
+}
