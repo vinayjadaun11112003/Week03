8f63d44f354980e98c9c9899bb04272ce802d818
vinayjadaun11112003 <vinayjadaun11112003@gmail.com>
2025-01-30 12:15:49 +0530

    task submitted to capgemini week03 day02 @30-01-2025

--- Day02/com/sample_problem_for_hashmap_and_hashing/two_sum_problem/TwoSum.java
+++ Day02/com/sample_problem_for_hashmap_and_hashing/two_sum_problem/TwoSum.java
@@ -2,51 +2,39 @@ package com.sample_problem_for_hashmap_and_hashing.two_sum_problem;
 
 import java.util.*;
 
+import java.util.HashMap;
+
 public class TwoSum {
 
-    // Linked list node to handle collisions using separate chaining
-    static class ListNode {
-        int index;
-        ListNode next;
-
-        ListNode(int index) {
-            this.index = index;
-            this.next = null;
-        }
-    }
-
-    // Hash map with an array of linked lists to handle collisions
     public static int[] twoSum(int[] nums, int target) {
-        // Create the hash map with size equal to the array length
-        ListNode[] map = new ListNode[nums.length];
+        // Create a HashMap to store the number and its index
+        HashMap<Integer, Integer> map = new HashMap<>();
 
+        // Iterate through the array
         for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
+            int complement = target - nums[i];  // Calculate the complement
 
-            // Check if the complement exists in the hash map
-            int hash = Math.abs(complement) % nums.length;
-            ListNode head = map[hash];
-            while (head != null) {
-                if (nums[head.index] == complement) {
-                    return new int[]{head.index, i};
-                }
-                head = head.next;
+            // Check if the complement is already in the map
+            if (map.containsKey(complement)) {
+                // If found, return the indices
+                return new int[] { map.get(complement), i };
             }
 
-            // If complement doesn't exist, insert the current element into the hash map
-            int index = Math.abs(nums[i]) % nums.length;
-            ListNode newNode = new ListNode(i);
-            newNode.next = map[index];
-            map[index] = newNode;
+            // Otherwise, add the current number and its index to the map
+            map.put(nums[i], i);
         }
-        return new int[]{};
+
+        // If no pair is found, return an empty array
+        return new int[] {};
     }
 
     public static void main(String[] args) {
+        // Test the function
         int[] nums = {2, 7, 11, 15};
         int target = 9;
 
         int[] result = twoSum(nums, target);
+
         if (result.length == 2) {
             System.out.println("Indices: " + result[0] + ", " + result[1]);
         } else {
@@ -54,4 +42,6 @@ public class TwoSum {
         }
     }
 }
+
+
 
